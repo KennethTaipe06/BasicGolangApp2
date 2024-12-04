190eca1dfdd08a84b86e8cc0166cceeb26f5b35f
Kenneth Taipe <kennethtaipe@gmail.com>
2024-12-04 16:08:03 -0500

    feat: :sparkles: Update helloWorldPage to return hostname

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
+    "os"
 )
 
 func helloWoldPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "It works 3/5 golang")
+    hostname, err := os.Hostname()
+    if err != nil {
+        http.Error(w, "Could not get hostname", http.StatusInternalServerError)
+        return
+    }
+    fmt.Fprintf(w, "Hostname: %s", hostname)
 }
 
 func main() {
-	http.HandleFunc("/", helloWoldPage)
-	http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/", helloWoldPage)
+    http.ListenAndServe(":8080", nil)
 }
